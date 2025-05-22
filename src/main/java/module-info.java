e785f01a67bb099929b5b89a268594b6759be69d
H2Q <hqh1hqh11@outlook.com>
2025-05-22 21:27:33 +0800

    1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module top.jwmc.kuri.ezdrawboard {
     requires javafx.fxml;
     requires org.controlsfx.controls;
     requires org.jetbrains.annotations;
+    requires java.desktop;
     exports top.jwmc.kuri.ezdrawboard.client;
     opens top.jwmc.kuri.ezdrawboard.client to javafx.fxml;
 }
