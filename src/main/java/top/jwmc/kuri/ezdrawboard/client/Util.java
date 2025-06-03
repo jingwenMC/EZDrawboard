2f14d642c3083bdf1196a87c5ea4fc2030bbba5d
jingwenMC <2313055824@qq.com>
2025-06-03 18:53:18 +0800

    Abstract

---
+++ src/main/java/top/jwmc/kuri/ezdrawboard/client/Util.java
@@ -0,0 +1,13 @@
+package top.jwmc.kuri.ezdrawboard.client;
+
+import java.io.File;
+
+public class Util {
+    public static String FILE_NAME = "tmp.png";
+    public void byteToFile(final byte[] data) {
+
+    }
+    public byte[] fileToByte() {
+        return null;
+    }
+}
