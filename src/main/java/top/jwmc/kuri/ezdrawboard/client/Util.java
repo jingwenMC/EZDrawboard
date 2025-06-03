c5afcea668e2499a21cdff7b3da0d1f6af3e6439
H2Q <hqh1hqh11@outlook.com>
2025-06-03 19:14:24 +0800

    1

--- src/main/java/top/jwmc/kuri/ezdrawboard/client/Util.java
+++ src/main/java/top/jwmc/kuri/ezdrawboard/client/Util.java
@@ -1,13 +1,27 @@
 package top.jwmc.kuri.ezdrawboard.client;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class Util {
     public static String FILE_NAME = "tmp.png";
     public void byteToFile(final byte[] data) {
-
+        try (FileOutputStream fos = new FileOutputStream(FILE_NAME)) {
+            fos.write(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
     public byte[] fileToByte() {
-        return null;
+        try {
+            File file = new File(FILE_NAME);
+            return Files.readAllBytes(file.toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
